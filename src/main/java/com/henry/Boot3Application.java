3a743f7b7b8d7e5875205aaa531f3f5a7bec2608
henry <kimdong.dev@gmail.com>
2024-03-23 14:13:51 +0900

    feat: 테스트 주석 추가. - ...test3

--- src/main/java/com/henry/Boot3Application.java
+++ src/main/java/com/henry/Boot3Application.java
@@ -7,7 +7,7 @@ public class Boot3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Boot3Application.class, args);
-        System.out.println("커밋앤푸쉬");
+        System.out.println("커밋앤푸쉬2");
     }
 
 }
