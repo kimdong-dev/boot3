285431412fd26447d13b4a8bfeed8a52654fe201
kimdong85 <ddongfay1024@gmail.com>
2024-03-23 18:27:24 +0900

    feat: hi

--- src/main/java/com/henry/Boot3Application.java
+++ src/main/java/com/henry/Boot3Application.java
@@ -7,7 +7,7 @@ public class Boot3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Boot3Application.class, args);
-        System.out.println("나는 워커1 임 hi");
+        System.out.println("나는 워커1 임 hi"); 
     }
 
 }
