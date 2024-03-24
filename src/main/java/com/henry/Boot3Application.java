d5a17e0ef3b7f327386578f0c202a1e01e88f339
kimdong85 <ddongfay1024@gmail.com>
2024-03-24 16:35:21 +0900

    feat: kimdong85

--- src/main/java/com/henry/Boot3Application.java
+++ src/main/java/com/henry/Boot3Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Boot3Application {
 
-    public static void main(String[] args) { // henry 주석
+    public static void main(String[] args) { // henry 주석. kimdong85
         SpringApplication.run(Boot3Application.class, args);
-        System.out.println("henry가 수정");
+        System.out.println("henry, kimdong85가 수정");
     }
 
 }
